00e80c422e5088b4a23630f097aefa9cbdfb5e16
Nosafrontdev <nosyblink@gmail.com>
2025-10-17 16:58:06 +0100

    added new feature on Card

--- MovieBox/src/components/MovieList.jsx
+++ MovieBox/src/components/MovieList.jsx
@@ -13,7 +13,7 @@ function MovieList() {
     return (  
     
         <section className='bg-gray-900 py-6 px-4 min-h-screen'>
-        <h1 className='text-3x1 font-bold text-white mb-6'> Trending Movies</h1>
+        <h1 className='text-3x1 font-bold text-white mb-6 bg-red-900'> Trending Movies</h1>
         {isError && <p className='text-red-500'> Something went wrong </p>}
         {isLoading && <p className='text-white'> Loading --------</p>}
         <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg: grid-cols-5 xl:grid-cols-6 gap-4'> 
@@ -23,9 +23,7 @@ function MovieList() {
         <img 
         className="w-full h-full object-cover"
         src = {`https://image.tmdb.org/t/p/w500${movie.poster_path}`} 
-        alt={movie.title}
-
-        />
+        alt={movie.title}/>
         </div>
     )}
         </div>
